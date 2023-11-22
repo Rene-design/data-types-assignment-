fb84ebf2480a4537c2b0aa033f9d5ad0bb9fd319
Rene-design <151048341+Rene-design@users.noreply.github.com>
2023-11-22 09:14:11 +0300

    Details.java

---
+++ Details.java
@@ -0,0 +1,13 @@
+public class Details{
+  public static void main(String[]args){
+
+      System.out.println("Group Members");
+
+ System.out.println("NAME:Irene Joseph");
+ System.out.println("2.ADMISSION NUMBER:22/03886");
+ System.out.println("3.COURSE: DIPLOMAIN INFORMATION TECHNOLOGY");
+ System.out.println("4.STAGE:THREE");
+ System.out.println("5.UNIT: JAVA PROGRAMING");
+
+}
+}
